5e21e1c18bab370a3cb806bd4c86e5bc4ddcbfea
charliechauri <me@charliechauri.com>
2020-05-23 18:22:58 -0500

    feat: generate basic server for static pages

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("entry point");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use("/", express.static("static/pages"));
+
+app.listen(PORT, function () {
+  console.log(`app running on port: ${PORT}`);
+});
